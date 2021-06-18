4f9dfc2760ff5938afe246455f0a7c2266ec898b
Fatimohkuforiji1 <fatimohkuforiji@yahoo.com>
2021-06-18 17:26:41 +0100

    creaye

--- server/api.js
+++ server/api.js
@@ -1,14 +1,13 @@
 import { Router } from "express";
 import { Pool } from "pg";
-
-
+import { cors } from "cors";
 
 
 const pool = new Pool({
   user: "Umm Muhammad",
   host: "localhost",
-  database: "",
-  password: "",
+  database: "modest_buffalo_quizz",
+  password: "akande1",
   port: 5432,
 });
 
@@ -29,28 +28,20 @@ router.get("/teachers", function (req, res) {
     .catch((e) => console.error(e));
 });
 
-router.post("teachers", (req, res) => {
-  const newTeacher = req.body.teacher;
-  console.log(newTeacher);
-  const teacherQuery = `INSERT INTO teacher(name, address) VALUES ($1, $2) RETURNING ID`;
+router.post("/teachers", (req, res) => {
+  const newTeacherName = req.body.name;
+  const newTeacherAddress = req.body.address;
+   const teacherQuery = `INSERT INTO teacher(name, address) VALUES ($1, $2) RETURNING ID`;
   const regExpression = /^[a-zA-Z0-9 -]{1,30}$/;
-if(!regExpression(newTeacher)){
+
+if(!regExpression.exec(newTeacherName)){
 	res.status(500).send(error)
 } else{
-  pool.query(teacherQuery, [newTeacher])
+  pool.query(teacherQuery, [newTeacherName, newTeacherAddress])
     .then((result) => res.status(201).send(result.rows[0]))
     .catch((error) => res.status(500).send(error));
 }
 });
 
-//  if (error) {
-//       res.status(500).send(error);
-//     } else {
-//       res.json(result.rows);
-
-// router.get("/test", (req, res) => {
-// 	const allTeachers = "SELECT * FROM teachers"
-//   res.json({ message: "Hello, world!" });
-// });
 
 export default router;
