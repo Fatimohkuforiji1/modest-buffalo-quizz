cbf5edc017827ed5316c610fc062838f7fa95a0a
ironkamosi <alintonchambers@gmail.com>
2021-06-22 18:11:52 +0200

    added seperate registers for users

--- client/src/App.js
+++ client/src/App.js
@@ -1,41 +1,37 @@
-import React, {useContext} from "react";
+import React, { useContext } from "react";
 import { AuthContext } from "./Context/AuthContext";
 import { Route, Switch } from "react-router-dom";
-import AllRegisterFiles from "./Component/allRegisterFiles";
 import About from "./pages/About";
 import Home from "./pages/Home";
+import TeacherRegistrationForm from "./Component/TeacherRegistrationForm";
+import StudentRegistrationForm from "./Component/StudentRegistrationForm";
 
 const App = () => {
-  const {isAuthenticated} = useContext(AuthContext);
-return(
-  <Switch>
-    {isAuthenticated?(
-      <>
-      <Route path="/protected" exact>
-      <Home />
-    </Route>
-    <Route path="/about/this/site">
-      <About />
-    </Route>
-    </> 
-    ):(
-	<AllRegisterFiles />
-      
-    )}
+  const { isAuthenticated } = useContext(AuthContext);
+  return (
+    <Switch>
+      {isAuthenticated ? (
+        <>
+          <Route path="/protected" exact>
+            <Home />
+          </Route>
+          <Route path="/about/this/site">
+            <About />
+          </Route>
+        </>
+      ) : (
+        <>
+          <Route path="/register/teacher">
+            <TeacherRegistrationForm />
+          </Route>
 
-    
-  </Switch>
-)
+          <Route path="/register/student">
+            <StudentRegistrationForm />
+          </Route>
+        </>
+      )}
+    </Switch>
+  );
 };
 
-
 export default App;
-
-// function App() {
-//   return (
-//     <div className="App">
-
-//      <AllLoginFiles/>
-//     </div>
-//   );
-// }
