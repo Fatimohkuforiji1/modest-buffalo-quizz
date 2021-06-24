64b8b6412164dad8ae97bc7681b2fc0617c6688b
Kos0606 <65927936+Kos0606@users.noreply.github.com>
2021-06-24 21:39:50 +0100

    Modified the route

--- server/api.js
+++ server/api.js
@@ -7,7 +7,26 @@ router.get("/", (_, res) => {
 	res.json({ message: "Hello, world!" });
 });
 
+router.post("/login/student", function (req, res) {
+  const {email, password} = req.body
+  pool
+    .query("SELECT * FROM student WHERE email=$1 AND password=$2", [email, password])
+    .then((result) => {
+      if (result.rowCount > 0) {
+        return res.status(200).send({
+          result: `SUCCESS`,
+          message: `User exists !`,
+        });
+      } else {
+        return res.status(400).send({
+          result: `ERROR`,
+          message: `User doesn't exist !`,
+        })
+      }
+    })
 
+      
+});
 router.get("/quizzes", (_, res) => {
   const sql = `SELECT * FROM quizzes`;
   pool.query(sql)
