77473473d5dcaa84073972377a52a6c5520bc596
Fatimohkuforiji1 <fatimohkuforiji@yahoo.com>
2021-06-25 15:00:23 +0100

    Rendered the layout component and ceated routes

--- client/src/App.js
+++ client/src/App.js
@@ -4,32 +4,66 @@ import { Route, Switch } from "react-router-dom";
 import AllRegisterFiles from "./Component/allRegisterFiles";
 import About from "./pages/About";
 import Home from "./pages/Home";
+import Register from "./Component/Register"
+import Login from "./Component/Login"
+import Layout from "./Component/Layout";
+
+  
 
 const App = () => {
   const {isAuthenticated} = useContext(AuthContext);
-return(
+return (
   <Switch>
-    {isAuthenticated?(
-      <>
-      <Route path="/protected" exact>
-      <Home />
-    </Route>
-    <Route path="/about/this/site">
-      <About />
-    </Route>
-    </> 
-    ):(
-	<AllRegisterFiles />
-      
-    )}
+    <Layout>
+      {isAuthenticated ? (
+        <>
+          <Route path="/protected" exact>
+            <Home />
+          </Route>
+          <Route path="/about/this/site">
+            <About />
+          </Route>
+        </>
+      ) : (
+        // <AllRegisterFiles />
+        <>
+          <Route path="/register">
+            <Register />
+          </Route>
 
-    
+          <Route path="/login">
+            <Login />
+          </Route>
+
+          <Route path="/" exact>
+            <Home />
+          </Route>
+        </>
+      )}
+    </Layout>
   </Switch>
-)
+);
 };
 
 
 export default App;
+
+
+
+
+
+
+//   <Switch>
+//     {isAuthenticated?(
+//       <>
+//       <Route path="/protected" exact>
+//       <Home />
+//     </Route>
+//     <Route path="/about/this/site">
+//       <About />
+//     </Route>
+// </>
+
 
 // function App() {
 //   return (
