49456d88921090daa18c9d7bc6b36be301a4050a
ironkamosi <alintonchambers@gmail.com>
2021-07-01 23:51:24 +0100

    Removed unused logic

--- client/src/App.js
+++ client/src/App.js
@@ -1,94 +1,58 @@
 import React, { useContext } from "react";
 import { AuthContext } from "./Context/AuthContext";
 import { Route, Switch } from "react-router-dom";
-// import AllRegisterFiles from "./Component/AllRegisterFiles";
 import About from "./pages/About";
 import Home from "./pages/Home";
-// import Register from "./Component/Register"
-import Login from "./Component/Login"
+import Login from "./Component/Login";
 import Layout from "./Component/Layout";
 import TeacherRegistrationForm from "./Component/TeacherRegistrationForm";
 import StudentRegistrationForm from "./Component/StudentRegistrationForm";
 import TeacherDashboard from "./Component/TeacherDashboard";
 import StudentDashboard from "./Component/StudentDashboard";
 
-
-  
-
 const App = () => {
-  const {isAuthenticated} = useContext(AuthContext);
-return (
-  <Switch>
-    <Layout>
-      {isAuthenticated ? (
-        <>
-          <Route path="/protected" exact>
-            <Home />
-          </Route>
-          <Route path="/about/this/site">
-            <About />
-          </Route>
-        </>
-      ) : (
-        // <AllRegisterFiles />
-        <>
-          {/* <Route path="/register">
-            <Register />
-          </Route> */}
+  const { isAuthenticated } = useContext(AuthContext);
+  return (
+    <Switch>
+      <Layout>
+        {isAuthenticated ? (
+          <>
+            <Route path="/protected" exact>
+              <Home />
+            </Route>
+            <Route path="/about/this/site">
+              <About />
+            </Route>
+          </>
+        ) : (
+          <>
+            <Route path="/login">
+              <Login />
+            </Route>
 
-          <Route path="/login">
-            <Login />
-          </Route>
+            <Route path="/" exact>
+              <Home />
+            </Route>
+            <Route path="/register/teacher">
+              <TeacherRegistrationForm />
+            </Route>
 
-          <Route path="/" exact>
-            <Home />
-          </Route>
-          <Route path="/register/teacher">
-            <TeacherRegistrationForm />
-          </Route>
+            <Route path="/register/student">
+              <StudentRegistrationForm />
+            </Route>
 
-          <Route path="/register/student">
-            <StudentRegistrationForm />
-          </Route>
+            <Route path="/dashboard/student">
+              <StudentDashboard />
+            </Route>
 
-          <Route path="/dashboard/student">
-            <StudentDashboard />
-          </Route>
-
-          <Route path="/dashboard/teacher">
-            <TeacherDashboard />
-          </Route>
-        </>
-      )}
-    </Layout>
-  </Switch>
-);
+            <Route path="/dashboard/teacher">
+              <TeacherDashboard />
+            </Route>
+          </>
+        )}
+      </Layout>
+    </Switch>
+  );
 };
 
 export default App;
-
-
-
-
-
-
-//   <Switch>
-//     {isAuthenticated?(
-//       <>
-//       <Route path="/protected" exact>
-//       <Home />
-//     </Route>
-//     <Route path="/about/this/site">
-//       <About />
-//     </Route>
-// </>
-
-
-// function App() {
-//   return (
-//     <div className="App">
-
-//      <AllLoginFiles/>
-//     </div>
-//   );
-// }
