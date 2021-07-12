7accd6f4390c34e1b34c41004519e3b0b85768dc
Fatimohkuforiji1 <fatimohkuforiji@yahoo.com>
2021-07-12 10:07:17 +0100

    created both teacher and student login  link

--- client/src/Component/AccountSuccess.js
+++ client/src/Component/AccountSuccess.js
@@ -1,6 +1,6 @@
 import React from "react";
 import { Link } from "react-router-dom";
-const AccountSuccess = () => {
+const AccountSuccess = ({role}) => {
   return (
     <div className="signup-success">
       <h1>CODE YOUR FUTURE</h1>
@@ -8,11 +8,19 @@ const AccountSuccess = () => {
         <div className="registered">
           <h1>Account Created</h1>
         </div>
-        <button>
-          <Link className="link" to="/login">
-            Login
-          </Link>
-        </button>
+        {role === "teacher" ? (
+          <button>
+            <Link className="link" to="/login">
+              Teacher Login
+            </Link>
+          </button>
+        ) : (
+          <button>
+            <Link className="link" to="/student-login">
+              Student Login
+            </Link>
+          </button>
+        )}
       </div>
     </div>
   );
